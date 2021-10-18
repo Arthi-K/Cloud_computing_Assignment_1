db7b7e4f52ca4f1e28baa1c0abfe37ddb4dc732d
Arthi-K <81531516+Arthi-K@users.noreply.github.com>
2021-10-17 21:29:36 -0700

    Update QEMU_FileIO_test.sh

---
+++ QEMU_FileIO_test.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+#
+file_mode='rndwr rndrw seqwr'
+tests=5
+
+for mode in $file_mode;  do
+
+for(( i = 1; i <= tests; i++ ))
+do
+	docker run csminpp/ubuntu-sysbench sysbench --num-threads=16 --test=fileio --file-total-size=1G --file-test-mode=$mode prepare
+	docker run csminpp/ubuntu-sysbench sysbench --num-threads=16 --test=fileio --file-total-size=1G --file-test-mode=$mode run 
+	docker run csminpp/ubuntu-sysbench sysbench --num-threads=16 --test=fileio --file-total-size=1G --file-test-mode=$mode cleanup
+	echo Test $i finished in QEMU 
+sync && sudo purge
+done
+done
